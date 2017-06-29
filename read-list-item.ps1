0b6970337ca7c8e081f902da562c807b8b7725c7
chinthaka321 <chinthaka321@yahoo.com>
2017-06-30 09:56:51 +1000

    Update read-list-item.ps1

--- read-list-item.ps1
+++ read-list-item.ps1
@@ -1,1 +1,17 @@
+if((Get-PSSnapin | Where {$_.Name -eq "Microsoft.SharePoint.PowerShell"}) -eq $null) {
+    Add-PSSnapin Microsoft.SharePoint.PowerShell;
+}
 
+$sourceWebURL = "http://url"
+$sourceListName = "ListName"
+
+$spSourceWeb = Get-SPWeb -site $sourceWebURL
+$spSourceList = $spSourceWeb.Lists[$sourceListName]
+#$spSourceItems = $spSourceList.GetItems()
+#$spSourceItems = $spSourceList.GetItemById("1")
+$spSourceItems = $spSourceList.Items | where {$_['Title'] -eq '44422'}
+
+$spSourceItems | ForEach-Object {
+    Write-Host $_['ID']
+    Write-Host $_['Business Unit']
+} 
